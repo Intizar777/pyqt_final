dabb1fad93ac920fe92475b610e8d98a4b88e96b
intizar777 <intizar.isaeva.2006@gmail.com>
2025-04-24 00:29:37 +0300

    ok2

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from PyQt6 import QtWidgets
 from PyQt6.QtWidgets import QMessageBox
 import MySQLdb as mdb
 import sys
-from new_window import Ui_MainWindow
+from main_window2 import Ui_MainWindow
 
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
@@ -30,6 +30,8 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.setWindowTitle("Калькулятор страховых взносов")
         self.load_positions()
         self.create_insurance_checks()
+        self.update_insurance_checks(0)
+        self.comboBox.currentIndexChanged.connect(self.update_insurance_checks)
         self.radioButton.setText(self.get_types_operation()[0][0])
         self.radioButton_2.setText(self.get_types_operation()[1][0])
 
@@ -48,23 +50,41 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         except mdb.Error as e:
             QMessageBox.critical(self, "Ошибка", f"Не удалось загрузить должности: {e}")
 
+    def update_insurance_checks(self, idx: int):
+        cursor = self.conn.cursor()
+        cursor.execute("SELECT id_insur_contr FROM Post_Contributions WHERE id_post = %s",
+                       (idx + 1,)
+        )
+        allowed_insurances_for_position = list(map(lambda x: int(x[0]), cursor.fetchall()))
+        print(allowed_insurances_for_position)
+        for checkbox in self.buttons.buttons():
+            checkbox_id = self.buttons.id(checkbox)
+            checkbox.setChecked(checkbox_id in allowed_insurances_for_position)
+            checkbox.setDisabled(True)
+
+
+
     def create_insurance_checks(self):
         try:
             cursor = self.conn.cursor()
             cursor.execute("SELECT id, contrib_name FROM Insurance_Contributions_Types")
-            buttons = QtWidgets.QButtonGroup(self)
-            buttons.setExclusive(False)
-            layout = QtWidgets.QVBoxLayout()
-            for contrib in cursor.fetchall():
+            insurance_checks = cursor.fetchall()
+            self.buttons = QtWidgets.QButtonGroup(self)
+            self.buttons.setExclusive(False)
+            for contrib in insurance_checks:
                 cb = QtWidgets.QCheckBox(contrib[1])
-                buttons.addButton(cb, contrib[0])
-                cb.setChecked(True)
+                cb.setStyleSheet("""
+                    QCheckBox {
+                        color: black;
+                    }
+                """)
+                self.buttons.addButton(cb, contrib[0])
                 cb.setProperty("contribution_id", contrib[0])
-                layout.addWidget(cb)
+                self.insurance_layout.addWidget(cb)
 
             if self.groupBox_2.layout():
                 QtWidgets.QWidget().setLayout(self.groupBox_2.layout())
-            self.groupBox_2.setLayout(layout)
+            self.groupBox_2.setLayout(self.insurance_layout)
 
         except mdb.Error as e:
             QMessageBox.critical(self, "Ошибка", f"Не удалось загрузить взносы: {e}")
@@ -94,15 +114,21 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def save_to_db(self):
         try:
             position_id = self.comboBox.currentData()
-            operation_type = 1 if self.radioButton_2.isChecked() else 2
+            operation_type = 1 if self.radioButton.isChecked() else 2
 
             cursor = self.conn.cursor()
+
+            cursor.callproc('CalculateSalaryAndContribution', (position_id, 0, 0))
+            cursor.execute("SELECT @_CalculateSalaryAndContribution_1, @_CalculateSalaryAndContribution_2")
+            salary_pay, insurance_sum = cursor.fetchone()
+
+            result = salary_pay if operation_type == 1 else insurance_sum
+
             cursor.execute("""
                 INSERT INTO Accrual (accrual_date, summa, id_oper, id_post)
                 VALUES (CURDATE(), 
-                (SELECT salary FROM Posts WHERE id = %s), 
-                1, %s)
-            """, (position_id, position_id))
+                %s, %s, %s)
+            """, (result, operation_type, position_id))
 
             accrual_id = cursor.lastrowid
             cursor.callproc('ProcessAccrualWithLogging', (accrual_id, operation_type, 0))
